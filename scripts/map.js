d47ebc9a60c23ab4defd4299606e896f02520dc9
Jack Dougherty <jack.dougherty@trincoll.edu>
2017-03-19 15:23:16 -0400

    changed point Title to Name

--- scripts/map.js
+++ scripts/map.js
@@ -123,7 +123,7 @@ $(window).on('load', function() {
 
       if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
-          .bindPopup("<b>" + point['Title'] + '</b><br>' +
+          .bindPopup("<b>" + point['Name'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
           point['Description']);
 
