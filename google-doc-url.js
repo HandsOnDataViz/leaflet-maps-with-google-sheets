dd92423c8f2722fc8486e640a4a21cb09ce2e7b6
franktoughmap <35256106+franktoughmap@users.noreply.github.com>
2018-01-09 11:29:41 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/10PMlGouwH5ZqFtlCGoknIsZzKva7ganeg4wYsBauI7Y/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/10PMlGouwH5ZqFtlCGoknIsZzKva7ganeg4wYsBauI7Y/edit#gid=794636164';
