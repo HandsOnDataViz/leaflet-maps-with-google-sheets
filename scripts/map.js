242295b8427fb0877eaeb7c4aa714dce069df44c
mallbn <44560785+mallbn@users.noreply.github.com>
2020-06-25 12:41:01 +0100

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -127,8 +127,8 @@ $(window).on('load', function() {
       if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
-          (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') + point['Name'] +
-          point['Description'] + point['Location']);
+          (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
+          point['Description']);
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
