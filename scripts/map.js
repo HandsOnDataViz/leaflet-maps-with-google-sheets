c4b4b18fe7789144e43bcfe3a3927353096e2baa
mallbn <44560785+mallbn@users.noreply.github.com>
2020-06-25 13:16:05 +0100

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -124,7 +124,7 @@ $(window).on('load', function() {
           point['Icon Color']
         );
 
-      if (point.Latitude !== '' && point.Longitude !== '') {
+       if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
